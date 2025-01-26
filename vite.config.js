8a2fc5342322c0fa048ab947b6c6a2b36c47ae99
sameersam648 <154335238+sameersam648@users.noreply.github.com>
2025-01-27 02:42:35 +0545

    first commit

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/money_tracker/", // Add your repository name here
+  base: "/Money_Tracker",
 });
